77c82a8a949a189450f71d64ba23f7a2e219876f
Shruti-kri08 <shrutiikumarii08@gmail.com>
2026-04-25 10:11:34 +0530

    adding count api

--- Models/Contact.js
+++ Models/Contact.js
@@ -3,8 +3,8 @@ const ContactSchema=mongoose.Schema({
     fullname:{type:String,require:true},
     phone:{type:Number,require:true},
     gender:{type:String,required:true},
-    userId:{type:String,required:true},
-    image:{type:String,required:true},
-    url:{type:String,required:true}
+    userId:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'User'},
+    imgId:{type:String},
+    imgUrl:{type:String}
 })
 module.exports=mongoose.model('Contact',ContactSchema)
